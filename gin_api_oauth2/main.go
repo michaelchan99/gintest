e61824deed05aed80319bdeead75ec15fa4ef27a
michaelchan99 <michaelchan99@gmail.com>
2019-12-17 18:51:25 +0800

    Initial update

--- gin_api_oauth2/main.go
+++ gin_api_oauth2/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"github.com/gin-gonic/gin"
+	"github.com/golang/glog"
 	ginglog "github.com/szuecs/gin-glog"
 	ginoauth2 "github.com/zalando/gin-oauth2"
 	"time"
@@ -16,4 +17,14 @@ func main() {
 	router.Use(ginglog.Logger(3 * time.Second))
 	router.Use(ginoauth2.RequestLogger([]string{"uid"}, "data"))
 	router.Use(gin.Recovery())
+
+	ginoauth2.VarianceTimer = 300 * time.Millisecond // defaults to 30s
+
+	public := router.Group("/api")
+	public.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Hello to public world"})
+	})
+
+	glog.Info("bootstrapped application")
+	router.Run(":8081")
 }
