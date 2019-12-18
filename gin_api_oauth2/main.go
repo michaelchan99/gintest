5533ff32fe4af7bb53a81411443ce5408d97e2f2
michaelchan99 <michaelchan99@gmail.com>
2019-12-18 19:17:21 +0800

    Change to Google Auth

--- gin_api_oauth2/main.go
+++ gin_api_oauth2/main.go
@@ -3,28 +3,44 @@ package main
 import (
 	"flag"
 	"github.com/gin-gonic/gin"
-	"github.com/golang/glog"
-	ginglog "github.com/szuecs/gin-glog"
-	ginoauth2 "github.com/zalando/gin-oauth2"
-	"time"
+	"github.com/zalando/gin-oauth2/google"
+	"net/http"
 )
 
 // https://github.com/zalando/gin-oauth2
 
 func main() {
+
+	redirectURL := "http://127.0.0.1:8081/auth/"
+	credFile := "./example/google/test-clientid.google.json" // you have to build your own
+	scopes := []string{
+		"https://www.googleapis.com/auth/userinfo.email",
+		// You have to select your own scope from here -> https://developers.google.com/identity/protocols/googlescopes#google_sign-in
+	}
+	secret := []byte("secret") //
+	sessionName := "goquestsession"
+
 	flag.Parse()
-	router := gin.New()
-	router.Use(ginglog.Logger(3 * time.Second))
-	router.Use(ginoauth2.RequestLogger([]string{"uid"}, "data"))
+
+	router := gin.Default()
+	// init settings for google auth
+	google.Setup(redirectURL, credFile, scopes, secret)
+	router.Use(google.Session(sessionName))
 	router.Use(gin.Recovery())
 
-	ginoauth2.VarianceTimer = 300 * time.Millisecond // defaults to 30s
+	router.GET("/login", google.LoginHandler)
 
-	public := router.Group("/api")
-	public.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Hello to public world"})
+	// protected url group
+	private := router.Group("/auth")
+	private.Use(google.Auth())
+	private.GET("/", UserInfoHandler)
+	private.GET("/api", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"message": "Hello from private for groups"})
 	})
 
-	glog.Info("bootstrapped application")
 	router.Run(":8081")
+}
+
+func UserInfoHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"Hello": "from private", "user": ctx.MustGet("user").(google.User)})
 }
